3dec63a7ffae90e1562b0bdcd787d450b0ac0461
Tarepan <tarepan5884@gmail.com>
2019-12-30 21:44:11 +0900

    Add automatic arXiv search

--- src/testAction.ts
+++ src/testAction.ts
@@ -30,14 +30,31 @@ async function run(): Promise<void> {
   );
 
   // add to storage with [candidate] status
-
-  // check for debug
-  //// for current view
-  console.log(
-    searchResults.map(paper => ({ id: paper.id, title: paper.title }))
-  );
-  console.log(storage);
-  console.log(newPapers);
+  if (newPapers.length > 0) {
+    const theNewPaper = newPapers[0];
+    storage.push({
+      id: theNewPaper.id,
+      status: "candidate"
+    });
+    // commit storage update
+    await octokit.repos
+      .createOrUpdateFile({
+        ...github.context.repo,
+        path: "arXivSearches.json",
+        message: `Add new arXiv search result ${theNewPaper.id}`,
+        content: new Buffer(JSON.stringify(storage)).toString("base64")
+      })
+      .catch(err => console.log(err));
+    // open candidate check issue
+    await octokit.issues
+      .create({
+        ...github.context.repo,
+        title: `'Voice Conversion' paper candidate ${theNewPaper.id}`,
+        body: `Please check whether this paper is about 'Voice Conversion' or not.\n## article info.\n- title: ${theNewPaper.title}\n- summary: ${theNewPaper.summary}\n- id: ${theNewPaper.id}\n## judge\nWrite 'confirmed' or 'excluded' in [] as comment.`
+      })
+      .catch(err => console.log(err));
+    // tweet candidate info
+  }
 }
 
 run();
