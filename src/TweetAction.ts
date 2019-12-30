cd349f42a773acd1705892ebfd9df159689eebde
Tarepan <tarepan5884@gmail.com>
2019-12-30 12:44:43 +0900

    Add debuger

--- src/TweetAction.ts
+++ src/TweetAction.ts
@@ -2,6 +2,7 @@ import * as core from "@actions/core";
 import { tweet } from "./twitter";
 
 (async (): Promise<void> => {
+  console.log("before tweet");
   await tweet(
     `hello, new world ! ${new Date().toTimeString()}`,
     core.getInput("twi-cons-key"),
@@ -10,11 +11,16 @@ import { tweet } from "./twitter";
     core.getInput("twi-token-secret")
   )
     .then(res => {
+      console.log("1st then in tweet");
       console.log(res.status);
       return res.text();
     })
-    .then(res => console.log(res))
+    .then(res => {
+      console.log("2nd then in tweet");
+      console.log(res);
+    })
     .catch(err => {
+      console.log("error in tweet");
       console.log(err);
       core.setFailed(err.message);
     });
