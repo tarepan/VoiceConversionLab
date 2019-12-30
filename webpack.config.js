e745ef2f54a0934076fd41c930fd488a71f7f1c3
Tarepan <tarepan5884@gmail.com>
2019-12-30 10:29:53 +0900

    Refactor with multiple output build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    testAction: "./src/testAction.js"
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 8000
+  }
+};
