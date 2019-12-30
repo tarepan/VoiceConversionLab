1c8015ee5b321365b01300f56d382d741543f2d6
Tarepan <tarepan5884@gmail.com>
2019-12-30 12:33:23 +0900

    Add tweet handling

--- src/TweetAction.ts
+++ src/TweetAction.ts
@@ -1,18 +1,21 @@
 import * as core from "@actions/core";
 import { tweet } from "./twitter";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  (async (): Promise<void> => {
-    console.log(`input twi-cons-key: ${core.getInput("twi-cons-key")}`);
-    await tweet(
-      new Date().toTimeString(),
-      core.getInput("twi-cons-key"),
-      core.getInput("twi-cons-secret"),
-      core.getInput("twi-token-key"),
-      core.getInput("twi-token-secret")
-    );
-  })();
-} catch (error) {
-  core.setFailed(error.message);
-}
+(async (): Promise<void> => {
+  await tweet(
+    `hello, new world ! ${new Date().toTimeString()}`,
+    core.getInput("twi-cons-key"),
+    core.getInput("twi-cons-secret"),
+    core.getInput("twi-token-key"),
+    core.getInput("twi-token-secret")
+  )
+    .then(res => {
+      console.log(res.status);
+      return res.text();
+    })
+    .then(res => console.log(res))
+    .catch(err => {
+      console.log(err);
+      core.setFailed(err.message);
+    });
+})();
