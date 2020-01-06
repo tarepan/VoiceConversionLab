7a1eedeafb91830a15aa7b634d1af55032d4b1fb
Tarepan <tarepan5884@gmail.com>
2020-01-06 23:17:05 +0900

    Add reply commnet Action

--- src/ConfirmationAction.ts
+++ src/ConfirmationAction.ts
@@ -24,84 +24,75 @@ async function run(): Promise<void> {
     const isC = c.exec(issueCommentPayload.comment.body);
     const isE = e.exec(issueCommentPayload.comment.body);
 
-    if (isC !== null) {
-      console.log("is [vclab::confirmed]");
-    } else if (isE !== null) {
-      console.log("is [vclab::excluded]");
-    } else {
-      console.log("Neither");
+    // make thunks toward contribution
+    if (isC !== null || isE !== null) {
+      const octokit = new github.GitHub(core.getInput("token"));
+      // reply thunks in comment
+      await octokit.issues
+        .createComment({
+          ...github.context.repo,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          issue_number: issueCommentPayload.issue.number,
+          body: `Thunk you very much for contribution!!\nYour judgement was refrected in arXivSearches.json, and will be used for VCLab's activity.\nThunk you so much!!`
+        })
+        .catch(err => core.setFailed(err));
+      // close the issue
+
+      // update store
+      //// fetch storage
+      // const octokit = new github.GitHub(core.getInput("token"));
+      // const contents = await octokit.repos.getContents({
+      //   ...github.context.repo,
+      //   path: "arXivSearches.json"
+      // });
+      // const storage: ArXivStorage = JSON.parse(
+      //   Buffer.from(
+      //     //@ts-ignore
+      //     contents.data.content,
+      //     //@ts-ignore
+      //     contents.data.encoding
+      //   ).toString()
+      // );
+      // commit storage update
+      // const blob = Buffer.from(JSON.stringify(storage));
+      // await octokit.repos
+      //   .createOrUpdateFile({
+      //     ...github.context.repo,
+      //     path: "arXivSearches.json",
+      //     message: `Add new arXiv search result ${theNewPaper.id}`,
+      //     content: blob.toString("base64"),
+      //     // @ts-ignore
+      //     sha: contents.data.sha
+      //   })
+      //   .catch(err => core.setFailed(err));
+
+      if (isC !== null) {
+        console.log("is [vclab::confirmed]");
+        // update store
+
+        // tweet confirmed paper
+        //   await tweet(
+        //     `[VC paper]\n"${theNewPaper.title}"\narXiv: ${theNewPaper.id}`,
+        //     core.getInput("twi-cons-key"),
+        //     core.getInput("twi-cons-secret"),
+        //     core.getInput("twi-token-key"),
+        //     core.getInput("twi-token-secret")
+        //   )
+        //     .then(res => {
+        //       console.log(res.status);
+        //       return res.text();
+        //     })
+        //     .catch(err => {
+        //       core.setFailed(err);
+        //     });
+        //   console.log("tweet created.");
+        // }
+      } else if (isE !== null) {
+        console.log("is [vclab::excluded]");
+        // update store
+      }
     }
   }
-
-  // fetch storage
-  // const octokit = new github.GitHub(core.getInput("token"));
-  // const contents = await octokit.repos.getContents({
-  //   ...github.context.repo,
-  //   path: "arXivSearches.json"
-  // });
-  // const storage: ArXivStorage = JSON.parse(
-  //   Buffer.from(
-  //     //@ts-ignore
-  //     contents.data.content,
-  //     //@ts-ignore
-  //     contents.data.encoding
-  //   ).toString()
-  // );
-
-  // find non-match (==new) arXivPaper
-  // const newPapers = searchResults.filter(paper =>
-  //   storage.every(record => record.id !== paper.id)
-  // );
-
-  // add to storage with [candidate] status
-  // if (newPapers.length > 0) {
-  //   const theNewPaper = newPapers[0];
-  //   storage.push({
-  //     id: theNewPaper.id,
-  //     status: "candidate"
-  //   });
-
-  // commit storage update
-  // const blob = Buffer.from(JSON.stringify(storage));
-  // await octokit.repos
-  //   .createOrUpdateFile({
-  //     ...github.context.repo,
-  //     path: "arXivSearches.json",
-  //     message: `Add new arXiv search result ${theNewPaper.id}`,
-  //     content: blob.toString("base64"),
-  //     // @ts-ignore
-  //     sha: contents.data.sha
-  //   })
-  //   .catch(err => core.setFailed(err));
-  // console.log("storage updated.");
-
-  // open candidate check issue
-  // await octokit.issues
-  //   .create({
-  //     ...github.context.repo,
-  //     title: `'Voice Conversion' paper candidate ${theNewPaper.id}`,
-  //     body: `Please check whether this paper is about 'Voice Conversion' or not.\n## article info.\n- title: **${theNewPaper.title}**\n- summary: ${theNewPaper.summary}\n- id: ${theNewPaper.id}\n## judge\nWrite 'confirmed' or 'excluded' in [] as comment.`
-  //   })
-  //   .catch(err => core.setFailed(err));
-  // console.log("issue created.");
-
-  // tweet candidate info
-  //   await tweet(
-  //     `[new VC paper candidate]\n"${theNewPaper.title}"\narXiv: ${theNewPaper.id}`,
-  //     core.getInput("twi-cons-key"),
-  //     core.getInput("twi-cons-secret"),
-  //     core.getInput("twi-token-key"),
-  //     core.getInput("twi-token-secret")
-  //   )
-  //     .then(res => {
-  //       console.log(res.status);
-  //       return res.text();
-  //     })
-  //     .catch(err => {
-  //       core.setFailed(err);
-  //     });
-  //   console.log("tweet created.");
-  // }
 }
 
 run();
